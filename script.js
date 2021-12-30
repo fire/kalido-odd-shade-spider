0381711249a69474f4375459fc6985096a4abde8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-29 18:53:57 -0800

    Remove landmarks.

--- script.js
+++ script.js
@@ -367,8 +367,6 @@ let videoElement = document.querySelector(".input_video"),
   guideCanvas = document.querySelector("canvas.guides");
 
 const onResults = (results) => {
-  // Draw landmark guides
-  drawResults(results);
   // Animate model
   animateVRM(currentVrm, results);
 };
@@ -388,54 +386,6 @@ holistic.setOptions({
 });
 // Pass holistic a callback function
 holistic.onResults(onResults);
-
-const drawResults = (results) => {
-  guideCanvas.width = videoElement.videoWidth;
-  guideCanvas.height = videoElement.videoHeight;
-  let canvasCtx = guideCanvas.getContext("2d");
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, guideCanvas.width, guideCanvas.height);
-  // Use `Mediapipe` drawing functions
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
-    color: "#00cff7",
-    lineWidth: 4,
-  });
-  drawLandmarks(canvasCtx, results.poseLandmarks, {
-    color: "#ff0364",
-    lineWidth: 2,
-  });
-  drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, {
-    color: "#C0C0C070",
-    lineWidth: 1,
-  });
-  if (results.faceLandmarks && results.faceLandmarks.length === 478) {
-    //draw pupils
-    drawLandmarks(
-      canvasCtx,
-      [results.faceLandmarks[468], results.faceLandmarks[468 + 5]],
-      {
-        color: "#ffe603",
-        lineWidth: 2,
-      }
-    );
-  }
-  drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {
-    color: "#eb1064",
-    lineWidth: 5,
-  });
-  drawLandmarks(canvasCtx, results.leftHandLandmarks, {
-    color: "#00cff7",
-    lineWidth: 2,
-  });
-  drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
-    color: "#22c3e3",
-    lineWidth: 5,
-  });
-  drawLandmarks(canvasCtx, results.rightHandLandmarks, {
-    color: "#ff0364",
-    lineWidth: 2,
-  });
-};
 
 // Use `Mediapipe` utils to get camera - lower resolution = higher fps
 const camera = new Camera(videoElement, {
